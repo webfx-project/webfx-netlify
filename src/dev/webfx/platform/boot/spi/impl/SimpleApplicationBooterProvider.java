78b38adf5a4ca47ced78c66188267737cb4f6c76
salmonb <maintainer@webfx.dev>
2025-11-03 18:13:02 +0000

    Update from https://github.com/webfx-demos/webfx-demo-demofx/commit/c2278083e1fd7180153c34ed3e3104263ba96b65

--- src/dev/webfx/platform/boot/spi/impl/SimpleApplicationBooterProvider.java
+++
@@ -1,9 +0,0 @@
-package dev.webfx.platform.boot.spi.impl;
-
-import dev.webfx.platform.boot.spi.ApplicationBooterProvider;
-
-/**
- * @author Bruno Salmon
- */
-public class SimpleApplicationBooterProvider implements ApplicationBooterProvider {
-}
